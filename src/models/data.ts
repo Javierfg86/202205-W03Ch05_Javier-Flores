59d78beff0f638acae792b9fe678e2240103a285
Javierfg86 <Javierfg86@outlook.es>
2022-05-29 23:56:22 +0200

    Add html files and scripts

--- src/models/data.ts
+++
@@ -1,9 +0,0 @@
-import { TaskModel } from './task.js';
-
-let tasks = [
-    new TaskModel('Hacer el header', 'Irene'),
-    new TaskModel('Hacer el footer', 'Fernamdo'),
-    new TaskModel('Hacer el main', 'Jose'),
-];
-tasks[0].isComplete = true;
-export const TASKS: Array<TaskModel> = tasks;
